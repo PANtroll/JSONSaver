a570ed5a02fedca3b23ad59d9c5e87e9b1756359
artur <zieloneimperium230@o2.pl>
2024-04-05 21:55:37 +0200

    add spring dependency

--- src/main/java/jaworski/artur/Main.java
+++ src/main/java/jaworski/artur/Main.java
@@ -1,5 +1,10 @@
 package jaworski.artur;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
     }
